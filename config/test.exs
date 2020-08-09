13284a8c09f7427375840514c54d7fdbfea5098d
George Shabunin <george.shabunin@gmail.com>
2020-08-09 16:00:30 +0100

    Setup circleci

--- config/test.exs
+++ config/test.exs
@@ -6,6 +6,7 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :comerier, Comerier.Repo,
+  username: "postgres",
   database: "comerier_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
