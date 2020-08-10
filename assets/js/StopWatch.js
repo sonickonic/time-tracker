ff033509d6c3e614a86cb2a23607ba1ef3d08d63
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-08-10 22:38:36 +0100

    Make stopwatch tick
    - Calculate time duration from startedAt time.
    - Render new duration evуry second

--- assets/js/StopWatch.js
+++ assets/js/StopWatch.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import moment from "moment";
+import momentDurationFormatSetup from "moment-duration-format";
+
+momentDurationFormatSetup(moment);
 
 import "../css/time-entry.scss";
 
-const StopWatch = ({ timeEntry }) => {
-  return <div className="time-entry__clock">{timeEntry.seconds}</div>;
+const Stopwatch = ({ timeEntry }) => {
+  const [duration, setDuration] = useState(0);
+
+  const updateDuration = () => {
+    const currentTime = moment();
+    const startTime = moment(timeEntry.startedAt);
+    const timeDiff = currentTime.diff(startTime);
+    const duration = moment.duration(timeDiff);
+
+    setDuration(duration);
+  };
+
+  useEffect(() => {
+    const id = setInterval(updateDuration, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return (
+    <div className="time-entry__clock">
+      {duration ? duration.format("h:mm") : ""}
+    </div>
+  );
 };
 
-StopWatch.propTypes = {
+Stopwatch.propTypes = {
   timeEntry: PropTypes.shape({
     id: PropTypes.number,
     started_at: PropTypes.string,
@@ -18,4 +43,4 @@ StopWatch.propTypes = {
   }).isRequired,
 };
 
-export default StopWatch;
+export default Stopwatch;
