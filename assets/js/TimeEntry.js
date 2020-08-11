52c89101ee672e056c56d3818d8162e213546775
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-08-11 19:28:26 +0100

    Display time entry data from server

--- assets/js/TimeEntry.js
+++ assets/js/TimeEntry.js
@@ -20,12 +20,16 @@ const TimeEntry = ({ timeEntry }) => {
       >
         <div className="time-entry__project-container">
           <h3 className="time-entry__title">project</h3>
-          <span className="time-entry__clint-name">MeUndies </span>
-          <span className="time-entry__project-name">- web redesign</span>
+          <span className="time-entry__clint-name">
+            {timeEntry.task.project.name}{" "}
+          </span>
+          <span className="time-entry__project-name">
+            - {timeEntry.task.project.client.name}
+          </span>
         </div>
         <div className="time-entry__task-container">
           <h3 className="time-entry__title">task</h3>
-          <span className="time-entry__task-name">Programming</span>
+          <span className="time-entry__task-name">{timeEntry.task.name}</span>
         </div>
         <div className="time-entry__time-container">
           <Stopwatch timeEntry={timeEntry} />
